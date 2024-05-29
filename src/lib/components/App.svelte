871ef14d6a97191f3376ac2f91d9c78834ebfb71
0xmugen <pierre.semanne@gmail.com>
2024-05-29 23:35:59 +0200

    feat: CCC

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+  import Ui from './UI/Ui.svelte';
   import { Canvas } from '@threlte/core'
+  import { World } from '@threlte/rapier'
   import Scene from './Scene.svelte'
 </script>
 
-<Canvas>
-  <Scene />
-</Canvas>
+<Ui />
+<div class="relative h-full w-full">
+  <Canvas>
+    <World>
+      <Scene />
+    </World>
+  </Canvas>
+</div>
