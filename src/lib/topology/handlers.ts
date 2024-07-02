cdabaa327e00cd3cdf3fe43e5b635a5483db93a7
droak <me+git@droak.sh>
2024-07-02 19:10:29 +0900

    preliminary refactoring for sprays

--- src/lib/topology/handlers.ts
+++ src/lib/topology/handlers.ts
@@ -1,9 +1,18 @@
 import { toString as uint8ArrayToString } from "uint8arrays/to-string";
 import { type ICanvas } from "./objects/canvas";
+import { OBJECT_ID, PRESENCE_GROUP, Player } from ".";
+
+export function handlePresenceMessages(e: any) {
+  if (e.detail.msg.topic !== PRESENCE_GROUP) return;
+  const input = uint8ArrayToString(e.detail.msg.data);
+  const player: Player = JSON.parse(input);
+
+  // TODO process what to do with other players information
+}
 
 // TODO: this should be superseded by wasm and main ts-topology library
 export const handleCanvasMessages = (canvas: ICanvas, e: any) => {
-  if (e.detail.msg.topic === "_peer-discovery._p2p._pubsub") return;
+  if (e.detail.msg.topic !== OBJECT_ID) return;
   const input = uint8ArrayToString(e.detail.msg.data);
   const message = JSON.parse(input);
   switch (message["type"]) {
@@ -19,8 +28,7 @@ export const handleCanvasMessages = (canvas: ICanvas, e: any) => {
 };
 
 function handleObjectUpdate(canvas: ICanvas, fn: string) {
-  // TODO: parse every brush type
-  // `paint(${node.getPeerId()}, [${[x, y]}], [${painting}])`
+  // TODO `addSpray(${new Date().getTime()},[${offset}],[${sprayType}])`,
   let args = fn.replace("paint(", "").replace(")", "").split(", ");
   let offset_p = args[1]
     .replace("[", "")
@@ -36,12 +44,8 @@ function handleObjectUpdate(canvas: ICanvas, fn: string) {
   const rgb: [number, number, number] = [rgb_p[0], rgb_p[1], rgb_p[2]];
 
   try {
-    canvas.paint(args[0], offset, rgb);
+    // canvas.paint(args[0], offset, rgb);
   } catch (e) {
     console.error(e);
   }
-}
-
-export function handlePresenceMessages(e: any) {
-  return e;
 }
--- src/lib/topology/index.ts
+++ src/lib/topology/index.ts
@@ -1,11 +1,24 @@
 import { TopologyNode } from "@topology-foundation/node";
+import { Vector3, Euler } from "three";
+import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
 import { Canvas, type ICanvas } from "./objects/canvas";
-import { Pixel } from "./objects/pixel";
-import { GCounter } from "@topology-foundation/crdt";
 import { handleCanvasMessages, handlePresenceMessages } from "./handlers";
+
+export type PlayerID = string;
+
+// TODO import it from src/lib/store/playersData.ts
+export interface Player {
+  id: PlayerID;
+  position: Vector3;
+  rotation: Euler;
+  state: "running" | "walking" | "jumping";
+}
 
 // exception just for the EthCC demo, the format is not this at all
 export const OBJECT_ID = "topology::counter_splash";
+export const PRESENCE_GROUP = "/counter-splash/presence/0.0.1";
+export const WIDTH = 4000;
+export const HEIGHT = 3000;
 
 export async function topologyInit() {
   // maybe add it to browser storage and
@@ -14,56 +27,45 @@ export async function topologyInit() {
   const node = new TopologyNode();
   await node.start();
 
-  // also add it to the handlers file
-  node.addCustomMessageHandler("/counter-splash/presence/0.0.1", (e) =>
-    handlePresenceMessages(e),
-  );
-
-  while (node.getPeers().length < 2) {
-    console.log("waiting for peers");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-  }
-
-  let canvas = getObject(node, OBJECT_ID);
-  while (canvas === null) {
-    console.log("waiting for canvas", node.getPeers());
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    await node.subscribeObject(OBJECT_ID);
-    canvas = getObject(node, OBJECT_ID);
-  }
-
-  // can add extra logic here
+  await node.subscribeObject(OBJECT_ID);
+  let canvas = new Canvas(node.getPeerId(), WIDTH, HEIGHT);
   node.addCustomGroupMessageHandler((e) => handleCanvasMessages(canvas, e));
-  // TODO store canvas in browser storage
 
-  return node;
+  node.addCustomGroup(PRESENCE_GROUP);
+  node.addCustomGroupMessageHandler((e) => handlePresenceMessages(e));
+
+  // TODO check best way to handle this
+  // and when to do full/partial merges (+ policy)
+  let extCanvas = getObject(node, OBJECT_ID);
+  while (extCanvas === null) {
+    extCanvas = getObject(node, OBJECT_ID);
+  }
+
+  canvas.merge(extCanvas);
 }
 
 export function getObject(node: TopologyNode, id: string): ICanvas | null {
   let object: any = node.getObject(id);
   if (!object) return null;
-
-  object["canvas"] = object["canvas"].map((x: any) =>
-    x.map((y: any) => {
-      y["red"] = Object.assign(new GCounter({}), y["red"]);
-      y["green"] = Object.assign(new GCounter({}), y["green"]);
-      y["blue"] = Object.assign(new GCounter({}), y["blue"]);
-      return Object.assign(new Pixel(node.getPeerId()), y);
-    }),
-  );
-
   return Object.assign(new Canvas(node.getPeerId(), 0, 0), object);
 }
 
-export function paintBrush1(
+export function addSpray(
   node: TopologyNode,
   canvas: ICanvas,
   offset: [number, number],
-  color: [number, number, number],
+  sprayType: number,
 ) {
-  canvas.paint(node.getPeerId(), offset, color);
+  canvas.addSpray(new Date().getTime(), offset, sprayType);
   node.updateObject(
     canvas,
-    `paint(${node.getPeerId()}, [${offset}], [${color}])`,
+    `addSpray(${new Date().getTime()},[${offset}],[${sprayType}])`,
+  );
+}
+
+export function sendPresence(node: TopologyNode, player: Player) {
+  node.sendGroupMessage(
+    "/counter-splash/presence/0.0.1",
+    uint8ArrayFromString(JSON.stringify(player)),
   );
 }
