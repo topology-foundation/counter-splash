9064e47ac1dbd7b0cad5760438199b953a20d449
0xmugen <pierre.semanne@gmail.com>
2024-07-02 16:23:52 +0200

    add: a few place holder images for sprays

--- src/lib/components/ui/sprayWheel.svelte
+++ src/lib/components/ui/sprayWheel.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
-    import { sprayWheel } from "$lib/store/player";
-  
-    const words = ["Word1", "Word2", "Word3", "Word4", "Word5", "Word6", "Word7", "Word8", "Word9"];
-  </script>
-  
-  {#if $sprayWheel}
-    <div class="relative h-screen w-full">
-      <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white p-4 rounded-lg shadow-lg z-50">
-        <div class="grid grid-cols-3 gap-2">
-          {#each words as word}
-            <div class="flex items-center justify-center bg-white text-black p-2 rounded shadow hover:bg-gray-300">
-              {word}
-            </div>
-          {/each}
-        </div>
+  import { sprayWheel } from "$lib/store/player";
+
+  const imageCount = 9;
+
+  function getImageSrc(index: number): string {
+    return `sprays/spray${index + 1}.png`;
+  }
+</script>
+
+{#if $sprayWheel}
+  <div class="relative h-screen w-full">
+    <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-blue-500 text-white p-4 rounded-lg shadow-lg z-50">
+      <div class="grid grid-cols-3 gap-2">
+        {#each Array(imageCount) as _, index}
+          <div class="flex items-center justify-center bg-white text-black p-2 rounded shadow hover:bg-gray-300">
+            <img src={getImageSrc(index)} alt="spray image" />
+          </div>
+        {/each}
       </div>
     </div>
-  {/if}
-  
+  </div>
+{/if}
